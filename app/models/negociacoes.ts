c31364e33f47a068212551a30d26a37cc01409a7
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-01-22 05:19:46 -0300

    Remodelação das classes e programação defensiva
    Remodelagem das classes utilizando as vantagens da linguagem TypeScript.
    
    Programação defensiva com a data, contornando os métodos de Date.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,13 @@
 import { Negociacao } from './negociacao.js';
 
 export class Negociacoes {
+    // private negociacoes: Negociacao[] = []; // this is the same as the line below
     private negociacoes: Array<Negociacao> = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
-
+    // lista(): readonly Negociacao[]   // this is the same as the line below
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,10 +1,12 @@
 export class Negociacoes {
     constructor() {
+        // private negociacoes: Negociacao[] = []; // this is the same as the line below
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // lista(): readonly Negociacao[]   // this is the same as the line below
     lista() {
         return this.negociacoes;
     }

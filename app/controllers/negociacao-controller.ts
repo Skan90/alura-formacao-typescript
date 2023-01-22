2bd10f2c15dfa9448f8b850950efa4a03f566c60
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-01-22 07:23:09 -0300

    Inspiração no React
    Criação com inspiração no React de templates declarativos e dinâmicos.
    
    Manipulação declarativa do DOM através de template.
    
    Template dinâmico
    Formatação de datas usando Intl.

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,22 +1,25 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
     private _inputData: HTMLInputElement;
     private _inputQuantidade: HTMLInputElement;
     private _inputValor: HTMLInputElement;
     private _negociacoes = new Negociacoes();
+    private _negociacoesView = new NegociacoesView('#negociacoesView');
 
     constructor() {
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
+        this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
         this._negociacoes.adiciona(negociacao);
-        console.log(this._negociacoes.lista());
+        this._negociacoesView.update(this._negociacoes);
         this.limparFormulario();
     }
 
@@ -25,10 +28,10 @@ export class NegociacaoController {
         const date = new Date(this._inputData.value.replace(expressaoRegular, ','));
         const quantidade = parseInt(this._inputQuantidade.value);
         const valor = parseFloat(this._inputValor.value);
-        
+
         return new Negociacao(date, quantidade, valor);
 
-        
+
     }
 
     limparFormulario(): void {
